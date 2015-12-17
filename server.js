0477351bc96fb859259b03e1b03f9907ff26b709
Mala <m.nimalasuriya@gmail.com>
2015-12-17 17:23:42 -0500

    changes for heroku deployment

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Dependencies
-// -----------------------------------------------------
 var express         = require('express');
 var mongoose        = require('mongoose');
 var port            = process.env.PORT || 3000;
@@ -8,26 +7,26 @@ var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
 var app             = express();
 
-// Express Configuration
-// -----------------------------------------------------
+
 // Sets the connection to MongoDB
-mongoose.connect("mongodb://localhost/timeCapsule");
+mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/timeCapsule");
 
 // Logging and Parsing
-app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
-app.use('/bower_components',  express.static(__dirname + '/bower_components')); // Use BowerComponents
-app.use(morgan('dev'));                                         // log with Morgan
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
-app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
-app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
 // Routes
-// ------------------------------------------------------
 require('./app/routes.js')(app);
 
 // Listen
-// -------------------------------------------------------
-app.listen(port);
-console.log('App listening on port ' + port);
+// app.listen(port);
+// console.log('App listening on port ' + port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
